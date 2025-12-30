6f8ccc33e4a5c7ffcd0c541886ddc87c36bc5ee8
afikmo26 <afik99mo@gmail.com>
2025-12-30 11:07:08 +0200

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>האתר עלה ✅</h1>
-      <p>אם אתה רואה את זה, דף הבית עובד.</p>
-    </main>
-  );
-}
